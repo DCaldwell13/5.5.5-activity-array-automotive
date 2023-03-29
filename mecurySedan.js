392c82c648e4178d3bf805758a2a01bd650b0019
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-03-28 19:11:54 -0700

    5.5.5-activity-array-automotive

--- mecurySedan.js
+++ mecurySedan.js
@@ -3,43 +3,43 @@ import Vehicle from "./vehicle.js/";
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage"); //defining the make,module,year,color, and mileage
 console.log(v.make) 
 class Car extends = vehicle {
     class Car extends = VehicleModule{
         constructor(make, model, year, color, mileage) {
             super(make, model, year, color, mileage);
-            this.maxPassengers = 5;
-            this.passenger = 0;
-            this.numberOfWheels = 4;
-            this.maxSpeed = 160;
-            this.fuel = 10;
-            this.scheduleService = false;
+            this.maxPassengers = 5; //setting the max number of passengers that can be inside the vehicle 
+            this.passenger = 0; //setting the number of passangers currently in the car
+            this.numberOfWheels = 4; //setting the number of wheels on the vehicle
+            this.maxSpeed = 160; //setting the max speed  
+            this.fuel = 10; //setting the amount of gallons of fuel that the cars currently has 
+            this.scheduleService = false; //stating that this vehicle is not scheduled for servicing
         }
 
         start() {
-            if (this.fuel > 0) {            
-                console.log("engine has started.");
-                return this.started = true
+            if (this.fuel > 0) {            //if the vehicles fuel is greater the 0 gallons the engine will start
+                console.log("engine has started."); 
+                return this.started = true // the engine has started because the vehilce has more then 0 gallons of fuel(true) 
             } else {
-                console.log("no fuel");
-                return this.started = false;
+                console.log("no fuel"); // the vehicle does not have fuel
+                return this.started = false; // the engine has not started because the amount of fuel inside of the car is 0 gallons
             }
         }
 
         loadPassenger(num) {
-            if (this.passenger < this.maxPassengers) {
-                if ((num + this.passenger) <= this.maxPassengers) {
-                    this.passenger = num;
-                    return this.passenger;               
+            if (this.passenger < this.maxPassengers) { //the number of passangers in this vehicle is less than the maximum compacity of passangers allowws
+                if ((num + this.passenger) <= this.maxPassengers) { // the number of passangers in the vehicle has to be less than or equal too the compacity of passangers allowed(5)
+                    this.passenger = num; //the total amount of passangers currently
+                    return this.passenger;              
                 } else {
-                    console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+                    console.log(this.model + " " + this.make + " not have enough space to take all passengers."); // if there is more than 5 passangers then it'll log "not enoungh space to take all passangers"
                 }
             } else {
-                console.log(this.model + " " + this.make + " is full");
+                console.log(this.model + " " + this.make + " is full"); 
             }
         }
-    
+        // invoking new Car, myCar.start(), myCar.loadPassangers(5), myCar.stop(), myCar.checkService()
         let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
         myCar.start()
         myCar.loadPassenger(5)
--- vehicle.js
+++ vehicle.js
@@ -1,5 +1,5 @@
 class Vehicle {
-    constructor(make, model, year, color, mileage) {
+    constructor(make, model, year, color, mileage) { //defining the Vehicle properties
         this.make = make;
         this.model = model;
         this.year = year;
@@ -20,7 +20,7 @@ class Vehicle {
    //         console.log("engine cannot start...");
    //     }
    // }
-    accelerate() {
+    accelerate() { //conditional statments stating wether the vehicle will accelerate based on fuel amount and how fast the fuel will run out and when the fuel does run out then the console will log out of fuel
         if (this.started) {
             if (this.fuel > 0) {
                 console.log(this.speed += 1);
@@ -33,7 +33,7 @@ class Vehicle {
             alert("You need to start the engine first.");
         }
     }
-    decelerate() {
+    decelerate() { // conditional statment stating that the vehicle started then it will slow down once the vehicle starts to run out of gas and once its completely out it will log "has stopped moving" then log "out of fuel"   
         if (this.started) {
             if (this.fuel > 0) {
                 if (this.speed > 0) {
@@ -55,32 +55,32 @@ class Vehicle {
         this.started = false;
     }
 
-    drive() {
+    drive() { //invoking driving
         accelerate();
     }
-    brake() {
+    brake() { //invoking brakes
         decelerate();
     }
 
-    autoPark()
+    autoPark() //invoking automatic parking 
     {
 
     }
 
-    autoDrive()
+    autoDrive() //invoking automatic driving
     {
       
     }
 
     typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) { //if the number of is 8 by 8 the this vehicle is a truck
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+        } else if (this.numberOfWheels == 4 && 4 == wheels) { // if not then its a car
             console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+        } else if (this.numberOfWheels == 2 && 2 == wheels) { //and if not a car than its a Bike
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
-            console.log("Unknown type of vehicle");
+            console.log("Unknown type of vehicle"); // and if neither of those than the it is a unknown type of vehicle
         }
     }
 }
